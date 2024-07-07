fab2d00ae3df46f860fb2eab9c8eed1398e05643
Qasim_wu20 <Qasim_wu20@STHQ012C-04>
2024-07-07 12:04:42 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/Pages/SignInPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Pages
+{
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("a");
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("s");
+
+        }
+    }
+}
